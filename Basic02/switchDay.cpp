db505c98eecb152024ca86edd04ec1c93f272326
Ananya Mohapatra <161347178+Ananya1830@users.noreply.github.com>
2024-03-19 23:42:26 +0530

    Displayed days using Switch statement.

---
+++ Basic02/switchDay.cpp
@@ -0,0 +1,35 @@
+// wap where user has to put numbers between 1 to 7 and 
+// print the day of the week according to the user input number
+
+#include <iostream>
+using namespace std;
+int main(){
+    int num;
+    cout << "Enter a number: ";
+    cin >> num;
+
+    switch(num) {
+        case 1:
+        cout << "Day is Sunday";
+        break;
+        case 2:
+        cout << "Day is Monday";
+        break;
+        case 3:
+        cout << "Day is Tuesday";
+        break;
+        case 4:
+        cout << "Day is Wedneday";
+        break;
+        case 5:
+        cout << "Day is Thursday";
+        break;
+        case 6:
+        cout << "Day is Friday";
+        break;
+        case 7:
+        cout << "Day is Saturday";
+        break;
+    }
+
+}
